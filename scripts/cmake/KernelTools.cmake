86b2c2daa9f2233d04e16aec626e23a5700421a6
Sine Striker <trueful@163.com>
2022-07-18 09:59:05 +0800

    modify cmake

--- scripts/cmake/KernelTools.cmake
+++ scripts/cmake/KernelTools.cmake
@@ -1,14 +1,14 @@
-# Add source files to target with specified scope, and output an object list.
-macro(chcore_target_sources_out_objects _target _scope _objects)
-    target_sources(${_target} ${_scope} ${ARGN})
+# Output an object list.
+macro(os_target_sources_out_objects _target _rel_dir _objects)
 
     if(NOT CMAKE_CURRENT_BINARY_DIR MATCHES "^${CMAKE_BINARY_DIR}/")
         message(
             FATAL_ERROR
-                "CMAKE_CURRENT_BINARY_DIR (${CMAKE_BINARY_DIR}) must be in CMAKE_BINARY_DIR (${CMAKE_BINARY_DIR})."
+                "CMAKE_CURRENT_BINARY_DIR (${CMAKE_CURRENT_BINARY_DIR}) must be in CMAKE_BINARY_DIR (${CMAKE_BINARY_DIR})."
         )
     endif()
-    string(REGEX REPLACE "^${CMAKE_BINARY_DIR}/" "" _curr_bin_rel_path
+
+    string(REGEX REPLACE "^${CMAKE_BINARY_DIR}/${_rel_dir}/" "" _curr_bin_rel_path
                          ${CMAKE_CURRENT_BINARY_DIR})
 
     foreach(_src ${ARGN})
@@ -16,6 +16,8 @@ macro(chcore_target_sources_out_objects _target _scope _objects)
             set(_obj_extension ${CMAKE_C_OUTPUT_EXTENSION})
         elseif(_src MATCHES "\.(s|S)$")
             set(_obj_extension ${CMAKE_ASM_OUTPUT_EXTENSION})
+        elseif(_src MATCHES "\.(asm)$")
+            set(_obj_extension ${CMAKE_ASM_NASM_OUTPUT_EXTENSION})
         elseif(_src MATCHES "\.(cpp|CPP|cxx|CXX|cc|CC)$")
             set(_obj_extension ${CMAKE_CXX_OUTPUT_EXTENSION})
         else()
@@ -24,7 +26,7 @@ macro(chcore_target_sources_out_objects _target _scope _objects)
         list(
             APPEND
             ${_objects}
-            CMakeFiles/${_target}.dir/${_curr_bin_rel_path}/${_src}${_obj_extension}
+            ${_rel_dir}/CMakeFiles/${_target}.dir/${_curr_bin_rel_path}/${_src}${_obj_extension}
         )
     endforeach()
 
@@ -33,29 +35,10 @@ macro(chcore_target_sources_out_objects _target _scope _objects)
 endmacro()
 
 # Add target to convert ELF kernel to binary image.
-function(chcore_objcopy_binary _kernel_target _binary_name)
+function(os_objcopy_binary _kernel_target _binary_name)
     add_custom_target(
         ${_binary_name} ALL
         COMMAND ${CMAKE_OBJCOPY} -O binary -S $<TARGET_FILE:${_kernel_target}>
                 ${_binary_name}
         DEPENDS ${_kernel_target})
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${_binary_name}
-            DESTINATION ${CMAKE_INSTALL_PREFIX})
-endfunction()
-
-# Add target to generate qemu emulation script.
-function(chcore_generate_emulate_sh _qemu _qemu_options)
-    set(qemu ${_qemu})
-    set(qemu_options ${_qemu_options})
-    configure_file(${CHCORE_PROJECT_DIR}/scripts/qemu/emulate.tpl.sh emulate.sh
-                   @ONLY)
-    unset(qemu)
-    unset(qemu_options)
-
-    install(PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/emulate.sh
-            DESTINATION ${CMAKE_INSTALL_PREFIX})
-    install(
-        PROGRAMS ${CMAKE_CURRENT_BINARY_DIR}/emulate.sh
-        DESTINATION ${CMAKE_INSTALL_PREFIX}
-        RENAME simulate.sh)
 endfunction()
